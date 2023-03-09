681337d1c60a6883bcddedf5b5775cb304926e75
mrgrinsven <mrgrinsven@gmail.com>
2023-03-09 03:28:01 +0100

    opdracht afgewerkt

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,55 @@
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
         // Hier mag je je code scrijven voor de hoofd-opdracht
+        int[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
+
+        class Translator {
+            Map<Integer, String> numericAlpha = new HashMap<Integer, String>();
+
+            public Translator(String[] alphabetic, int[] numeric) {
+                for (int i = 0; i < numeric.length; i++) {
+                    numericAlpha.put(numeric[i], alphabetic[i]);
+                }
+            }
+
+            public String translate(Integer number) {
+                return numericAlpha.get(number);
+            }
+        }
+
+        Translator translator = new Translator(alphabetic, numeric);
+        boolean play = true;
+        String ongeldig = "ongeldige invoer";
+        Scanner scanner = new Scanner(System.in);
+
+        while (play) {
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+
+            if (input.equals("x")) {
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Type een cijfer in van 0 t/m 9");
+                int number = scanner.nextInt();
+                scanner.nextLine();
+                if (number < 10 && number >= 0) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + number + " is " + result);
+                } else {
+                    System.out.println(ongeldig);
+                }
+            } else {
+                System.out.println(ongeldig);
+            }
+        }
+
 
         /* deze regel mag je weg halen voor de bonus opdracht. Onderstaande code is voor de bonus opdracht.
         HashSet<Integer> secretnumber = randomnumbergenerator();
